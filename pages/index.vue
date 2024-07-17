75f913423dc801d134bd03222137c3737f804fd1
fkkmemi <fkkmemi@gmail.com>
2024-07-17 22:32:41 +0900

    1. 개발환경

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const count = ref(333)
+
+const ars = [
+  1, 2, 3, 4, 6, 6, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 44, 6, 6, 6, 6, 6, 6, 4, 4,
+  4, 4, 4, 4, 4, 4, 4, 44,
+]
+</script>
+
+<template>
+  <div>
+    <a>jiji </a>
+    <strong>{{ count }}</strong>
+    <button @click="count++">up</button>
+  </div>
+</template>
